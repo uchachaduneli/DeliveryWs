da587cb3041804c3cbe5ec7f0f4baf033124c562
giorgitabatadze <giotabatadze1998@gmail.com>
2022-10-30 22:04:48 +0400

    modified Courier Check In Out entity

--- src/main/java/ge/bestline/delivery/ws/entities/CourierCheckInOut.java
+++ src/main/java/ge/bestline/delivery/ws/entities/CourierCheckInOut.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -23,6 +24,8 @@ public class CourierCheckInOut {
     private Integer id;
     private String carNumber;
     private Integer odometer;
+    @NotNull
+    private boolean isChekIn;
     @ManyToOne(cascade = CascadeType.PERSIST, optional = false)
     @NotFound(action = NotFoundAction.IGNORE)
     private User courier;
