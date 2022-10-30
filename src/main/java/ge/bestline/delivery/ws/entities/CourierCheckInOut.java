d95e52069dbe13dc2339ef082b08ae75dfcff8c6
giorgitabatadze <giotabatadze1998@gmail.com>
2022-10-30 21:35:01 +0400

    added Courier Check In Out

---
+++ src/main/java/ge/bestline/delivery/ws/entities/CourierCheckInOut.java
@@ -0,0 +1,49 @@
+package ge.bestline.delivery.ws.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CourierCheckInOut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String carNumber;
+    private Integer odometer;
+    @ManyToOne(cascade = CascadeType.PERSIST, optional = false)
+    @NotFound(action = NotFoundAction.IGNORE)
+    private User courier;
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private Date operationTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private Date updatedTime;
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private Date createdTime;
+
+    @PrePersist
+    protected void onCreate() {
+        createdTime = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedTime = new Date();
+    }
+}
